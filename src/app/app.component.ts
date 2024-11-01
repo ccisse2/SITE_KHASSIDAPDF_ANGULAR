67d18e6776de8c220456c838d85e7927fca7fea1
ccisse2 <cheikh.cisse2024@campus-eni.fr>
2024-11-02 00:39:00 +0100

    premiere commit avec la pagination est les card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CarouselComponent} from './composant/carousel/carousel.component';
+import {NabBarComponent} from './composant/nab-bar/nab-bar.component';
+import {CardKhassidaComponent} from './composant/card-khassida/card-khassida.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarouselComponent, NabBarComponent, CardKhassidaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
