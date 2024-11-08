0a0ebf489185f90c14e62bfca914d03b93d4e6d5
ccisse2 <cheikh.cisse2024@campus-eni.fr>
2024-11-08 19:56:39 +0100

    integrations des liens réseau sociaux

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import {Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+  private searchQuerySource = new Subject<string>();
 
   constructor() { }
 
-  private searchQuerySource = new Subject<string>();
-  searchQuery$ = this.searchQuerySource.asObservable();
+  // Déborde de 300 ms avant d'émettre chaque requête
+  searchQuery$ = this.searchQuerySource.asObservable().pipe(
+    debounceTime(1200), // Ajustez le délai de débordement en fonction de vos besoins
+    distinctUntilChanged() // éviter les appels répétés si la recherche n'a pas changé
+  );
 
   emitSearchQuery(query: string): void {
     this.searchQuerySource.next(query);
